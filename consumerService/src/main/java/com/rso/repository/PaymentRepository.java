51fdb7c9adf71f9e2bcb3eeeb329e40c3a6f3788
UtheMan <mik.umanski@gmail.com>
2019-05-21 18:58:44 +0200

    #17 changed endpoints to match payment/offer service specification

--- consumerService/src/main/java/com/rso/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rso.repository;
-
-import com.rso.model.Payment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PaymentRepository extends CrudRepository <Payment, Long> {
-}
