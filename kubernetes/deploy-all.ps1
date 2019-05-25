613498088de0e59eac775dd5c70c82906256efcd
kamdibus <kam.dibus@gmail.com>
2019-05-25 15:31:33 +0200

    auth policy

--- kubernetes/deploy-all.ps1
+++ kubernetes/deploy-all.ps1
@@ -1,4 +1,4 @@
-#kubectl apply -f testDeploy/testDeploy-deployment.yaml
-#kubectl apply -f testDeploy/testDeploy-lb.yaml
+kubectl apply -f testDeploy/testDeploy-deployment.yaml
+kubectl apply -f testDeploy/testDeploy-lb.yaml
 kubectl apply -f consumerService/consumer-deployment.yaml
 kubectl apply -f consumerService/consumer-lb.yaml
