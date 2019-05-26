d1a1842a669be51305d439712cc8a5b81ab82d23
kamdibus <kam.dibus@gmail.com>
2019-05-26 14:54:21 +0200

    update delete script with payment service

--- kubernetes/delete-deploy.ps1
+++ kubernetes/delete-deploy.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-kubectl delete deployment consumerservice testdeploy
-kubectl delete service consumer-lb testdeploy-lb
+kubectl delete deployment consumerservice testdeploy payment
+kubectl delete service consumer-lb testdeploy-lb payment-lb
 kubectl delete gateway http-gateway
 kubectl delete virtualservice external-services
