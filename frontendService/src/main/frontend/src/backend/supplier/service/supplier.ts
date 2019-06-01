dad6caea03fd54e0920dc85c7e51321b9d731314
drrefactor <drrefactor@gmail.com>
2019-06-01 12:12:24 +0200

    frontend environment config

--- frontendService/src/main/frontend/src/backend/supplier/service/supplier.ts
+++ frontendService/src/main/frontend/src/backend/supplier/service/supplier.ts
@@ -2,7 +2,7 @@ import { delay } from "../../common/utils/delay";
 import { UploadResponse } from "../model/Upload";
 import { OfferResponse, OfferPayload, OfferHistoryResponse, OfferStatus } from "../model/Offer";
 import Axios from 'axios';
-import { PAYMENT_URL } from "../../../environment";
+import { OFFERS_URL } from "../../../environment";
 
 export const SupplierService = {
   uploadInvoice: async function(fileName: string, fileData: File): Promise<UploadResponse> {
@@ -13,17 +13,17 @@ export const SupplierService = {
   },
   postOffer: async function(offer: OfferPayload): Promise<OfferResponse> {
     return Axios
-      .post(PAYMENT_URL + '/offers/', offer)
+      .post(OFFERS_URL + '/offers/', offer)
       .then(r => r.data)
   },
   getOffers: async function(): Promise<OfferResponse[]> {
     return Axios
-      .get(PAYMENT_URL + '/offers/')
+      .get(OFFERS_URL + '/offers/')
       .then(r => r.data)
   },
   getOffersHistory: async function(): Promise<OfferResponse[]> {
     return Axios
-      .get(PAYMENT_URL + '/offers/')
+      .get(OFFERS_URL + '/offers/')
       .then(r => r.data)
   }
 }
