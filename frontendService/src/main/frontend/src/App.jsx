4db84eed054e7fed19b7ba680a8f251ef992af48
DrRefactor <DrRefactor@gmail.com>
2019-06-02 21:49:47 +0200

    Frontend auth0 (#61)
    Add auth0
    
    * Static files nginx serve fix
    
    * frontend environment config
    
    * frontend api urls
    
    * Auth0 WIP
    
    * Auth0, preserve frontend route during auth process
    
    * auth0 frontend cleanup
    
    * Update frontend dependencies
    
    * Frontend react-router-dom typings dependency

--- frontendService/src/main/frontend/src/App.jsx
+++ frontendService/src/main/frontend/src/App.jsx
@@ -3,14 +3,38 @@ import styled from 'styled-components';
 import Router from './Router';
 import { MuiPickersUtilsProvider } from 'material-ui-pickers';
 import MomentUtils from '@date-io/moment';
+import { withRouter } from 'react-router';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+  }
+
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback') {
+      this.setState({ checkingSession:false });
+      return;
+    }
+
+    // try {
+    //   await auth0Client.silentAuth();
+    //   this.forceUpdate();
+    // }
+    // catch (err) {
+    //   console.log(err);
+    // }
+    this.setState({ checkingSession:false });
+  }
+
   render() {
     return (
       <MuiPickersUtilsProvider utils={MomentUtils}>
         <Container>
           <Body>
-            <Router />
+            <Router checkingSession={this.state.checkingSession} />
           </Body>
         </Container>
       </MuiPickersUtilsProvider>
@@ -29,4 +53,4 @@ const Body = styled.div`
   flex: 1 0;
 `;
 
-export default App;
+export default withRouter(App);
--- frontendService/src/main/frontend/src/backend/supplier/service/supplier.ts
+++ frontendService/src/main/frontend/src/backend/supplier/service/supplier.ts
@@ -3,6 +3,7 @@ import { UploadResponse } from "../model/Upload";
 import { OfferResponse, OfferPayload, OfferHistoryResponse, OfferStatus } from "../model/Offer";
 import Axios from 'axios';
 import { OFFERS_URL } from "../../../environment";
+import auth0Client from "../../auth/auth0";
 
 export const SupplierService = {
   uploadInvoice: async function(fileName: string, fileData: File): Promise<UploadResponse> {
@@ -11,19 +12,34 @@ export const SupplierService = {
       id: 11
     };
   },
-  postOffer: async function(offer: OfferPayload): Promise<OfferResponse> {
+  acceptOffer: async function(offerId: string) {
+    return this.updateOffer(offerId, OfferStatus.ACCEPTED)
+  },
+  rejectOffer: async function(offerId: string) {
+    return this.updateOffer(offerId, OfferStatus.REJECTED)
+  },
+  updateOffer: async function(offerId: string, status: OfferStatus) {
+    const headers = auth0Client.getAuthHeaders();
     return Axios
-      .post(OFFERS_URL + '/', offer)
+      .patch(OFFERS_URL + '/' + offerId, { status }, { headers })
+      .then(r => r.data)
+  },
+  postOffer: async function(offer: OfferPayload): Promise<OfferResponse> {
+    const headers = auth0Client.getAuthHeaders();
+    return Axios
+      .post(OFFERS_URL + '/', offer, { headers })
       .then(r => r.data)
   },
   getOffers: async function(): Promise<OfferResponse[]> {
+    const headers = auth0Client.getAuthHeaders();
     return Axios
-      .get(OFFERS_URL + '/')
+      .get(OFFERS_URL + '/', { headers })
       .then(r => r.data)
   },
   getOffersHistory: async function(): Promise<OfferResponse[]> {
+    const headers = auth0Client.getAuthHeaders();
     return Axios
-      .get(OFFERS_URL + '/')
+      .get(OFFERS_URL + '/', { headers })
       .then(r => r.data)
   }
 }
