6dad2541a84890199650c88885517e0b8b8296a1
UtheMan <mik.umanski@gmail.com>
2019-06-03 20:42:30 +0200

    user metadata fetch for frontend

--- frontendService/src/main/frontend/src/environment.js
+++ frontendService/src/main/frontend/src/environment.js
@@ -5,3 +5,7 @@ export const OFFERS_URL = process.env.NODE_ENV === 'production' ?
 export const PAYMENTS_URL = process.env.NODE_ENV === 'production' ?
   process.env.REACT_APP_PROD_PAYMENTS_URL :
   process.env.REACT_APP_DEV_PAYMENTS_URL
+
+export const USERS_URL = process.env.NODE_ENV === 'production' ?
+    process.env.REACT_APP_PROD_USERS_URL :
+    process.env.REACT_APP_DEV_USERS_URL
